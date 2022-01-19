0e0e1a14453d8de619c7e1aad44ac438b456b683
Albin <albinjiji4@gmail.com>
2022-01-19 16:50:01 +0530

    final commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,89 @@
-import "./App.css";
+import React, { useState, useEffect } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
-function App() {
-  return <div className="App">Hello</div>;
-}
+import "./App.css";
+import MovieList from "./components/MovieList";
+import MovieListHeading from "./components/MovieListHeading";
+import SearchBox from "./components/SearchBox";
+import AddWatchList from "./components/AddWatchList";
+import RemoveWatchList from "./components/RemoveWatchList";
+
+const App = () => {
+  const [movies, setMovies] = useState([]);
+  const [favourites, setFavourites] = useState([]);
+  const [searchValue, setSearchValue] = useState("");
+
+  const getMovieRequest = async (searchValue) => {
+    const url = `http://www.omdbapi.com/?s=${searchValue}&apikey=263d22d8`;
+
+    const response = await fetch(url);
+    const responseJson = await response.json();
+
+    if (responseJson.Search) {
+      setMovies(responseJson.Search);
+    }
+  };
+
+  useEffect(() => {
+    getMovieRequest(searchValue);
+  }, [searchValue]);
+
+  useEffect(() => {
+    const movieFavourites = JSON.parse(
+      localStorage.getItem("react-movie-app-favourites")
+    );
+
+    if (movieFavourites) {
+      setFavourites(movieFavourites);
+    }
+  }, []);
+
+  const saveToLocalStorage = (items) => {
+    localStorage.setItem("react-movie-app-favourites", JSON.stringify(items));
+  };
+
+  const addFavouriteMovie = (movie) => {
+    const newFavouriteList = [...favourites, movie];
+    setFavourites(newFavouriteList);
+    saveToLocalStorage(newFavouriteList);
+  };
+
+  const removeFavouriteMovie = (movie) => {
+    const newFavouriteList = favourites.filter(
+      (favourite) => favourite.imdbID !== movie.imdbID
+    );
+
+    setFavourites(newFavouriteList);
+    saveToLocalStorage(newFavouriteList);
+  };
+
+  return (
+    <div className="container-fluid movie-app">
+      <div className="movie-heading">
+        <MovieListHeading heading="My WatchList" className="movie-head" />
+        <h5 className="sub-head">
+          Search to add Movies or TV Shows to your WatchList
+        </h5>
+        <SearchBox searchValue={searchValue} setSearchValue={setSearchValue} />
+      </div>
+      <div className="row">
+        <MovieList
+          movies={movies}
+          handleFavouritesClick={addFavouriteMovie}
+          favouriteComponent={AddWatchList}
+        />
+      </div>
+      <div className="row d-flex align-items-center mt-4 mb-4">
+        <MovieListHeading heading="WatchList" />
+      </div>
+      <div className="row">
+        <MovieList
+          movies={favourites}
+          handleFavouritesClick={removeFavouriteMovie}
+          favouriteComponent={RemoveWatchList}
+        />
+      </div>
+    </div>
+  );
+};
 
 export default App;
--- src/components/SearchBox.jsx
+++ src/components/SearchBox.jsx
@@ -1,14 +1,17 @@
 import React from "react";
+import "./SearchBox.css";
 
 const SearchBox = (props) => {
   return (
-    <div className="col col-sm-4">
-      <input
-        className="form-control"
-        value={props.value}
-        onChange={(event) => props.setSearchValue(event.target.value)}
-        placeholder="Type to search..."
-      ></input>
+    <div className="container">
+      <div className="search">
+        <input
+          className="input"
+          value={props.value}
+          onChange={(event) => props.setSearchValue(event.target.value)}
+          placeholder="Type to search..."
+        ></input>
+      </div>
     </div>
   );
 };
